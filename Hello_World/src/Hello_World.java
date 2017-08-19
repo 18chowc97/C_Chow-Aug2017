29d8d42f14e2cfe47c589ee5f6824e823a90c0eb
18chowc97 <31104832+18chowc97@users.noreply.github.com>
2017-08-18 20:31:05 -0700

    Hello_World

--- Hello_World/src/Hello_World.java
+++ Hello_World/src/Hello_World.java
@@ -4,10 +4,10 @@ public class Hello_World {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		/**
-		 * Write a description of class Hello_world here.
+		 * First program test.
 		 * 
 		 * @author (Carl C) 
-		 * @version (a version number or a date)
+		 * @version (8/18/17)
 		 */
 		        System.out.println("Hello World! From Carl"); //Display the string
 		    }
