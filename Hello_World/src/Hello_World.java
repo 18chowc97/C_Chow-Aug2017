e4e8ee4fb6989cb77f4f1e7f0ccc0de2f5e437b9
18chowc97 <18chowc97@millshighsch.org>
2017-08-21 08:53:03 -0700

    New Local

--- Hello_World/src/Hello_World.java
+++ Hello_World/src/Hello_World.java
@@ -4,10 +4,10 @@ public class Hello_World {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		/**
-		 * First program test.
+		 * First program test. Local vs Cloud
 		 * 
 		 * @author (Carl C) 
-		 * @version (8/18/17)
+		 * @version (8/18/17-8/22/17)
 		 */
 		        System.out.println("Hello World! From Carl"); //Display the string
 		    }
