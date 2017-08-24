68dfd5297c228e69607666cb9c2e85caad5b5f4d
18chowc97 <18chowc97@millshighsch.org>
2017-08-24 08:28:55 -0700

    Project2

---
+++ Project2/src/Project2.java
@@ -0,0 +1,13 @@
+//Carl Chow
+//August 24, 2017
+//Project 2 - Use print and println
+public class Project2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi there");
+		System.out.println("hi there");
+		System.out.println("hi there");
+	}
+
+}
