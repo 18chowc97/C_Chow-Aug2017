8daa330f02e8661edac1e398c9411ea8cb24125b
18chowc97 <18chowc97@millshighsch.org>
2017-08-28 08:46:23 -0700

    Format vs printline

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -4,7 +4,9 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Test
-		System.out.print("Test line break \nTest");
+		System.out.println("Test line break \nTest");
+		System.out.println(5%4);
+		System.out.println("5%");
 	}
 
 }
