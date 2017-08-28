f695b249236c736fa42dc5c29d5f0f640a2693a4
18chowc97 <18chowc97@millshighsch.org>
2017-08-28 08:31:56 -0700

    Tabs-Pikachu

--- Pikachu/src/Pikachu.java
+++ Pikachu/src/Pikachu.java
@@ -5,8 +5,8 @@ public class Pikachu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Pikachu welcomes you to the world of Pokemon!");
-		System.out.println("\t\t  (\\__/)\n\t\t  (o^.^)\n\t\tz(_(\")(\")");
+		System.out.println("\tPikachu welcomes you to the world of Pokemon!");
+		System.out.println("\t\t\t  (\\__/)\n\t\t\t  (o^.^)\n\t\t\tz(_(\")(\")");
 	}
 
 }
