96eddce0df1afe66347104fd73c1d521df3b1ca7
18chowc97 <18chowc97@millshighsch.org>
2017-08-28 08:19:34 -0700

    Pikachu v1

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,10 @@
+
+public class Pikachu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Pikachu welcomes you to the world of Pokemon!");
+		System.out.println("\t\t\t  (\\__/)\n\t\t\t  (o^.^)\n\t\t\tz(_(\")(\")");
+	}
+
+}
