7e41712fcd8de020d9fc9352f603f93c4699927b
18chowc97 <18chowc97@millshighsch.org>
2017-08-29 08:07:32 -0700

    Printf testing

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -4,9 +4,12 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Test
+		int n = 13;
+		int n2 = 14;
+		
 		System.out.println("Test line break \nTest");
 		System.out.println(5%4);
-		System.out.println("5%");
+		System.out.printf("Number one is %d and number two is %d", n, n2);
 	}
 
 }
