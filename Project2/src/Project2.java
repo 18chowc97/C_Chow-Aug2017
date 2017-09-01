65d8c494921bd9f8beeb671d424d88351f93ed06
18chowc97 <18chowc97@millshighsch.org>
2017-09-01 08:15:06 -0700

    Static vs Nonstatic

--- Project2/src/Project2.java
+++ Project2/src/Project2.java
@@ -2,11 +2,16 @@
 //August 24, 2017
 //Project 2 - Use print and println
 public class Project2 {
+	public static int Add (int n, int n2) {
+		int sum = n + n2;
+		return sum;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hi there");
-		System.out.println("hi there");
+		int a = 1, b = 2;
+		int sum = Add(a, b);
+		System.out.println(sum);
 		System.out.println("hi there");
 	}
 
