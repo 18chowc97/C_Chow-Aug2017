c6bd562a07da2a641e235942e62d8b70d73442b4
18chowc97 <31104832+18chowc97@users.noreply.github.com>
2017-10-02 18:15:47 -0700

    Cleaning

--- QuadraticDescriber/src/QuadraticClient.java
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -1,7 +1,9 @@
+
 //Carl Chow
 //This is the client side of the Quadratic Describer.
 //9/22/17
 import java.util.*;
+
 public class QuadraticClient {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -12,15 +14,13 @@ public class QuadraticClient {
 		System.out.println("Welcome to Quadratic Describer!");
 		do {
 			System.out.println("What are the values of a, b, and c?");
-			System.out.print("a: ");
-			String A = console.nextLine();
 			while (test == 0) {
+				System.out.print("a: ");
+				String A = console.nextLine();
 				try {
 					a = Double.parseDouble(A);
 					if (a == 0) {
 						System.out.println("ERROR: It can't be a linear function! Try another value.");
-						System.out.print("a: ");
-						A = console.nextLine();
 					} else {
 						test++;
 					}
@@ -30,8 +30,6 @@ public class QuadraticClient {
 						System.out.println("Do you want to continue? (Type quit to end.)");
 						input = console.nextLine().toLowerCase();
 						if (input.equals("yes")) {
-							System.out.print("a: ");
-							A = console.nextLine();
 							break;
 						} else if (input.equals("quit")) {
 							test = 5;
@@ -42,69 +40,63 @@ public class QuadraticClient {
 					}
 				}
 			}
-			if (test == 1) {
+			while (test == 1) {
 				System.out.print("b: ");
-				while (true) {
-					String B = console.nextLine();
-					try {
-						b = Double.parseDouble(B);
-						test++;
-						break;
-					} catch (NumberFormatException exception) {
-						System.out.println("ERROR: Not a valid number: "+B);
-						while (true) {
-							System.out.println("Do you want to continue? (Type quit to end.)");
-							input = console.nextLine().toLowerCase();
-							if (input.contains("yes")) {
-								System.out.print("b: ");
-								break;
-							} else if (input.contains("quit")) {
-								test = 5;
-								break;
-							} else {
-								System.out.println("Not a valid response.");
-							}
+				String B = console.nextLine();
+				try {
+					b = Double.parseDouble(B);
+					test++;
+					break;
+				} catch (NumberFormatException exception) {
+					System.out.println("ERROR: Not a valid number: " + B);
+					while (true) {
+						System.out.println("Do you want to continue? (Type quit to end.)");
+						input = console.nextLine().toLowerCase();
+						if (input.contains("yes")) {
+							break;
+						} else if (input.contains("quit")) {
+							test = 5;
+							break;
+						} else {
+							System.out.println("Not a valid response.");
 						}
 					}
 				}
 			}
-			if (test == 2) {
+			while (test == 2) {
 				System.out.print("c: ");
-				while (true) {
-					String C = console.nextLine();
-					try {
-						c = Double.parseDouble(C);
-						test++;
-						break;
-					} catch (NumberFormatException exception) {
-						System.out.println("ERROR: Not a valid number: " + C);
-						while (true) {
-							System.out.println("Do you want to continue? (Type quit to end.)");
-							input = console.nextLine().toLowerCase();
-							if (input.equals("yes")) {
-								System.out.print("c: ");
-								break;
-							} else if (input.equals("quit")) {
-								test = 5;
-								break;
-							} else {
-								System.out.println("Not a valid response.");
-							}
+				String C = console.nextLine();
+				try {
+					c = Double.parseDouble(C);
+					test++;
+					break;
+				} catch (NumberFormatException exception) {
+					System.out.println("ERROR: Not a valid number: " + C);
+					while (true) {
+						System.out.println("Do you want to continue? (Type quit to end.)");
+						input = console.nextLine().toLowerCase();
+						if (input.equals("yes")) {
+							break;
+						} else if (input.equals("quit")) {
+							test = 5;
+							break;
+						} else {
+							System.out.println("Not a valid response.");
 						}
 					}
 				}
-				Quadratic.quadrDescriber(a, b, c);
-				System.out.println("Do you want to continue? (Type \"quit\" to exit.)");
-				input = console.nextLine().toLowerCase();
-				while (test == 3) {
-					if (input.equals("yes")) {
-						test = 0;
-					} else if (input.equals("quit")) {
-						test = 6;
-					} else {
-						System.out.println("Not a valid input. Try again.");
-						input = console.nextLine().toLowerCase();
-					}
+			}
+			Quadratic.quadrDescriber(a, b, c);
+			System.out.println("Do you want to continue? (Type \"quit\" to exit.)");
+			input = console.nextLine().toLowerCase();
+			while (test == 3) {
+				if (input.equals("yes")) {
+					test = 0;
+				} else if (input.equals("quit")) {
+					test = 6;
+				} else {
+					System.out.println("Not a valid input. Try again.");
+					input = console.nextLine().toLowerCase();
 				}
 			}
 		} while (test < 5);
