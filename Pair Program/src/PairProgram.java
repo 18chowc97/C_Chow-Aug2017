c30ecd5d2a0f8d86b27d3ed0f29feb5dd0a912c3
18chowc97 <31104832+18chowc97@users.noreply.github.com>
2017-10-03 16:45:32 -0700

    Continues Ask

--- Pair Program/src/PairProgram.java
+++ Pair Program/src/PairProgram.java
@@ -1,3 +1,7 @@
+
+//Carl Chow
+//This program takes a set of integer inputs until a person types "done" and prints certain calculations.
+//10/3/17
 import java.util.*;
 
 public class PairProgram {
@@ -18,40 +22,43 @@ public class PairProgram {
 			input = console.next();
 			if (!input.equals("done")) {
 				try {
-				inputp = Integer.parseInt(input);
-				}
-				catch(NumberFormatException exception) {
-					throw new InputMismatchException("Invalid Input");
-				}
-				if (counter == 1) {
-					max = inputp;
-					min = inputp;
-				} else {
-					if (inputp > max) {
+					inputp = Integer.parseInt(input);
+					if (counter == 1) {
 						max = inputp;
-					} else if (inputp < min) {
 						min = inputp;
-					}
-				}
-				counter++;
-				if (inputp % 2 == 0) {
-					evensum += inputp;
-					if (evenmax == 1) {
-						evenmax = inputp;
 					} else {
-						if (evenmax < inputp) {
-							evenmax = inputp;
+						if (inputp > max) {
+							max = inputp;
+						} else if (inputp < min) {
+							min = inputp;
 						}
 					}
+					counter++;
+					if (inputp % 2 == 0) {
+						evensum += inputp;
+						if (evenmax == 1) {
+							evenmax = inputp;
+						} else {
+							if (evenmax < inputp) {
+								evenmax = inputp;
+							}
+						}
+					}
+				} catch (NumberFormatException exception) {
+					System.out.println("Invalid input. Try again");
 				}
 			}
+
 		} while (!input.equals("done"));
-		System.out.println("Number of numbers: " + (counter - 1));
-		System.out.println("Largest Number: " + max);
-		System.out.println("Smallest Number: " + min);
-		System.out.println("Sum of evens: " + evensum);
-		System.out.println("Largest Even Number: " + evenmax);
+		if (counter == 1) {
+			System.out.println("No numbers entered.");
+		} else {
+			System.out.println("Number of numbers: " + (counter - 1));
+			System.out.println("Largest Number: " + max);
+			System.out.println("Smallest Number: " + min);
+			System.out.println("Sum of evens: " + evensum);
+			System.out.println("Largest Even Number: " + evenmax);
+		}
 		console.close();
 	}
-
 }
