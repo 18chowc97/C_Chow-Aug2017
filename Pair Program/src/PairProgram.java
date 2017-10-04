66e585b1720040865a82ef31da0239a67fc6bb09
18chowc97 <31104832+18chowc97@users.noreply.github.com>
2017-10-03 23:25:35 -0700

    Comments

--- Pair Program/src/PairProgram.java
+++ Pair Program/src/PairProgram.java
@@ -9,19 +9,22 @@ public class PairProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Enter in your integers here. (Enter \"done\" once you are done entering numbers.)");
+		//Instead of asking for the number of values to enter, the user enters values until
+		//he/she enters "done", at which point the program stops taking values and calculates.
 		Scanner console = new Scanner(System.in);
 		String input = "";
 		int inputp = 0;
 		int evensum = 0;
 		int counter = 1;
 		int max = 0;
-		int evenmax = 1;
+		int evenmax = 0;
 		int min = 0;
 		do {
 			System.out.print(counter + ": ");
 			input = console.next();
 			if (!input.equals("done")) {
 				try {
+					//Checks if input is an integer. 
 					inputp = Integer.parseInt(input);
 					if (counter == 1) {
 						max = inputp;
@@ -36,12 +39,10 @@ public class PairProgram {
 					counter++;
 					if (inputp % 2 == 0) {
 						evensum += inputp;
-						if (evenmax == 1) {
+						if (evenmax == 0) {
 							evenmax = inputp;
-						} else {
-							if (evenmax < inputp) {
-								evenmax = inputp;
-							}
+						} else if (evenmax < inputp){
+							evenmax = inputp;
 						}
 					}
 				} catch (NumberFormatException exception) {
@@ -49,7 +50,7 @@ public class PairProgram {
 				}
 			}
 
-		} while (!input.equals("done"));
+		} while (!input.equals("done")); //Continues if user does not enter "done".
 		if (counter == 1) {
 			System.out.println("No numbers entered.");
 		} else {
