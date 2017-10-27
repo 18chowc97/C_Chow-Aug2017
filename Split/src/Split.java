94dc18b7f4ee7d89e049e4e36b34d80f073502e7
18chowc97 <18chowc97@millshighsch.org>
2017-10-27 08:54:21 -0700

    Debugging

--- Split/src/Split.java
+++ Split/src/Split.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 
 public class Split {
 	public static String breadSplit(String s) {
-		// Your task:
 				/*
 				 * Write a method that take in a string like
 				 * "applespineapplesbreadlettucetomatobaconmayohambreadcheese" describing a
@@ -13,11 +12,18 @@ public class Split {
 		String[] split = s.split("bread");
 		String answer = "Contents of sandwich: ";
 		if(s.substring(s.length()-5, s.length()).equals("bread")) {
+			//Checked if the input ends in "bread" since .split behaves differently with "bread" in the front than in back.
+			if(split.length < 2) {
+				return "Illegal sandwich!";
+			}
 			for(int i = 1;i<split.length;i++) {
 				answer=answer+split[i];
 			}
 		}
 		else {
+			if(split.length < 3) {
+				return "Illegal Sandwich!";
+			}
 			for(int i = 1;i<split.length-1;i++) {
 				answer=answer+split[i];
 			}
@@ -25,25 +31,31 @@ public class Split {
 		return answer;
 	}
 	public static String spaceSplit(String s) {
-		// Your task pt 2:
-				/*
+				/* pt.2
 				 * Write a method that take in a string like
-				 * "apples pineapples bread lettus tomato bacon mayo ham bread cheese"
+				 * "apples pineapples bread lettuce tomato bacon mayo ham bread cheese"
 				 * describing a sandwich use String.split to split up the sandwich at the
 				 * spaces, " ", and return what's in the middle of the sandwich and ignores
 				 * what's on the outside Again, what if it's a fancy sandwich with multiple
 				 * pieces of bread?
 				 */
 		int startIndex = -1;
-		String answer = "";
+		String answer = "Contents of sandwich: ";
 		String[] split = s.split(" ");
 		for(int i = 0;startIndex == -1;i++) {
-			if (split[i].equals("bread")) {
-				startIndex = i;
+			try {
+				if (split[i].equals("bread")) {
+					startIndex = i;
+				}
+			}catch(ArrayIndexOutOfBoundsException exception){
+				return "Illegal Sandwich!";
 			}
 		}
 		for(int i = split.length -1;i>=0;i--) {
 			if(split[i].equals("bread")) {
+				if(startIndex==i-1 || startIndex == i) {
+					return "Illegal Sandwich!";
+				}
 				for(int j = startIndex + 1;j < i;j++) {
 					if(!split[j].equals("bread")) {
 						answer += split[j] + ", ";
@@ -52,7 +64,7 @@ public class Split {
 				i = -1;
 			}
 		}
-		return "Contents of sandwich: " + answer.substring(0, answer.length()-2);
+		return answer.substring(0, answer.length()-2);
 	}
 	public static void main(String[] args) {
 		// String.split();
@@ -64,14 +76,14 @@ public class Split {
 		// Example 2: "I really like really red apples"split("really")
 		// it will split at the word "really" and return an array of ["I "," like ","
 		// apples!"]
-
 		// play around with String.split! what happens if you "I reallyreally like
 		// apples".split("really") ?
-		String word = "breadlettucetomatobaconmayohamcheesebreadstuffbread";
-		String spaceWord = "apples bread lettuce mayo bread stuff cheese bread stuff";
+		String word = "lettucetomatobaconmayohamcheesestuffham";
+		String spaceWord = "a bread b c bread d bread e f";
 		String[] array = word.split("bread");
 		String[] arrayS = spaceWord.split(" ");
-		System.out.println("really I reallyreallyreally like apples really.".split("really"));
+		System.out.println(Arrays.toString("really I reallyreallyreally like really red apples really.".split("really")));
+		System.out.println(Arrays.toString("reallyIreallyreallyreallylikereallyredapplesreally.".split("really"))+"\n");
 		System.out.println(Arrays.toString(array));
 		System.out.println(breadSplit(word));
 		System.out.println(Arrays.toString(arrayS));
