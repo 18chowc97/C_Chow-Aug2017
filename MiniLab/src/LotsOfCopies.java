565b628837d648361b937f55215aa115f345f91a
18chowc97 <18chowc97@millshighsch.org>
2017-11-02 08:52:35 -0700

    Done

--- MiniLab/src/LotsOfCopies.java
+++ MiniLab/src/LotsOfCopies.java
@@ -6,24 +6,34 @@ public class LotsOfCopies {
 		String strMain = "APCS";
 		int[] arrMain = {1, 2, 3, 4 ,5};
 		changeMe(num, strMain, arrMain);
+		//Thought String would also be changed in method.
+		//Only arrMain was changed by method, reference semantics.
 		System.out.println(num);
 		System.out.println(strMain);
 		System.out.println(Arrays.toString(arrMain));
 		int a = 1, b = 2;
 		change(a, b);
+		//Does not change anything, since primitives use value semantics
 		System.out.println(a +", "+b);
 		String A = "a"; 
 		String B = "b";
 		change(A, B);
 		System.out.println(A + B);
-		int[] first = {1,2};
+		//Even though Strings are objects, they use value semantics, so did not change in the method.
+		int[] first = {1, 2};
 		int[] second = {3, 4};
 		change(first, second);
+		//Does not switch the arrays, impossible to change in a method with different references
 		System.out.println(Arrays.toString(first) + ", "+ Arrays.toString(second));
 		temp = first;
 		first = second;
 		second = temp;
+		//However, works if directly assigning arrays to new reference.
+		//The new reference works if in main.
 		System.out.println(Arrays.toString(first) + ", "+ Arrays.toString(second));
+		add(first);
+		//Adding works, because it does not change reference in a method.
+		System.out.println(Arrays.toString(first));
 	}
 	public static void changeMe(int x, String str, int[] arr) {
 		x++;
@@ -41,8 +51,13 @@ public class LotsOfCopies {
 		y = temp;
 	}
 	public static void change(int[] x, int[] y) {
-		int[] temp = x;
+		int[] tem = x;
 		x = y;
-		y = temp;
+		y = tem;
+	}
+	public static void add(int[] x) {
+		for (int i = 0; i< x.length;i++) {
+			x[i] ++;
+		}
 	}
 }
