75d08d856c9f053cdf176fd8c2ff7a322623ba99
18chowc97 <31104832+18chowc97@users.noreply.github.com>
2018-02-11 12:18:35 -0800

    More PM

---
+++ Prisms.zip_expanded/Prisms/Cylinder.java
@@ -0,0 +1,20 @@
+/* Write this concrete (non-abstract) class called Cylinder that extends Prism.  
+ * It has a private field (radius) and inherits its height from its superclass, Prism 
+ *    (because all prisms have a height).
+ * Provide a constructor and the methods required by its abstract superclass.  
+ */
+
+public class Cylinder extends Prism{
+	private double radius;
+	public Cylinder( double r, double h ) {
+		super(h);
+		radius = r;
+	}
+	public double calcAreaOfBase() {
+		return 3.14159 * radius * radius;
+	}
+	public double calcPerimeter() {
+		return 2 * 3.14159 * radius;
+	}
+	
+}
---
+++ Prisms.zip_expanded/Prisms/RectangularPrism.java
@@ -0,0 +1,22 @@
+/* Write this concrete (non-abstract) class called RectangularPrism.  
+ * It has 2 private fields (length and width).
+ * It inherits its height from its superclass, Prism (because all prisms have a height).
+ * Provide a constructor and the methods required by its abstract superclass.  
+ */
+
+public class RectangularPrism extends Prism{
+	private double length;
+	private double width;
+	public RectangularPrism(double w, double l, double h) {
+		super(h);
+		length = l;
+		width = w;
+	}
+	public double calcAreaOfBase() {
+		return length * width;
+	}
+	public double calcPerimeter() {
+		return 2 * (length + width);
+	}
+}
+
